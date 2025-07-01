618ab2816d4fcf2fb00684e67a6ad88c48c7cfa1
iamsjeevan <71625285+iamsjeevan@users.noreply.github.com>
2025-07-01 17:21:03 +0530

    Add new C file with improved link creation logic and output; include symlink functionality and create t3.txt as a symlink to t2.txt

--- 4a.c
+++ 4a.c
@@ -1,29 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<stdlib.h>
-#include<fcntl.h>
 #include<unistd.h>
-#include<sys/stat.h>
-#include<sys/types.h>
-int main(int argc,char *argv[]){
-if(argc==3){
-if((link(argv[1],argv[2]))==0){
-printf("Hard link created\n");
-}
-else{
-}
-}
+int main(int argc,char* argv[]){
+    if(argc ==3){
+        if(link(argv[1],argv[2])==0){
+            printf("hard link created from %s to %s\n", argv[1], argv[2]);
 
-printf("Hard link error\n");
+        }
 
-else if(argc==4){
-if((symlink(argv[2],argv[3]))==0){
-printf("Soft link created\n");
-}
-else{
-}
-}
-
-printf("Soft link error\n");
-
-return 0;
+    }
+    if(argc ==4){
+        if(symlink(argv[2],argv[3])==0){
+            printf("symbolic link created from %s to %s\n", argv[2], argv[3]);
+        } 
+    
+    }
+    return 0;
 }
